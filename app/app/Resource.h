4e76d5c88d7195920c19527ebbe6b2cd49059c69
DenysPostolnyi <postlden44@gmail.com>
2023-05-01 09:40:32 +0300

    created menue ABOUT_GAME

--- app/app/Resource.h
+++ app/app/Resource.h
@@ -1,6 +1,6 @@
-﻿//{{NO_DEPENDENCIES}}
-// Включаемый файл, созданный в Microsoft Visual C++.
-// Используется app.rc
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by app.rc
 //
 #define IDC_MYICON                      2
 #define IDD_APP_DIALOG                  102
@@ -11,6 +11,9 @@
 #define IDC_APP                         109
 #define IDR_MAINFRAME                   128
 #define IDI_ICON1                       129
+#define IDR_MENU1                       132
+#define ID_32771                        32771
+#define IDABOUT_GAME                    32772
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -18,8 +21,8 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        132
-#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32773
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
